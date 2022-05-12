f190aff7074f62783b791400e7628972cf6c469d
r_takaba <r_takaba@satt.jp>
2022-05-12 18:05:57 +0900

    ファイルアップロード処理を修正

--- FileUpload.js
+++ FileUpload.js
@@ -1,25 +1,28 @@
 function readFile(input) {
-  let file = input.files[0];
+  const files = input.files;
+  const imageList = document.querySelector(".uploadImageArea");
 
-  let reader = new FileReader();
-  console.log(file)
-  
-  reader.onload = function() {
-    const uploadImageArea = document.querySelector('.uploadImageArea')
-    uploadImageArea.innerHTML = `<img src="${reader.result}" alt="アップロードされた画像" id="uploadImage" onclick="deleteImage()"/>`
-  };  
-  reader.onerror = function() {
-    console.log(reader.error);
-  };
-  
-  reader.readAsDataURL(file);
+  Array.from(files).forEach(element => {
+    let reader = new FileReader(); 
+    reader.readAsDataURL(element);
 
+    reader.onload = function(e) {
+      let img = document.createElement("img");
+      img.setAttribute('src', e.target.result)
+      img.setAttribute('id', "uploadImage")
+      img.setAttribute('onclick', "deleteImage(this)")
+      imageList.appendChild(img);
+    }  
+    reader.onerror = function() {
+      console.log(reader.error);
+    };
+  })
+ 
 }
-
-function deleteImage() {
-  let isDelete = window.confirm('削除しますか？')
+function deleteImage(img) {
+  let isDelete = window.confirm("画像を削除しますか？");
   if(isDelete) {
-    document.querySelector('.uploadImageArea').innerHTML = ""
-  }else {
-  }
+    const uploadImageArea = document.querySelector('.uploadImageArea');
+    uploadImageArea.removeChild(img);
+  }    
 }
